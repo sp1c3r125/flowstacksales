b4e7ee032d2e354c08066c7ed6b1e04cd5f80ddc
Sp1c3r125 <your-email@gmail.com>
2026-03-19 14:07:48 +0800

    Resolve proposal export and handoff conflicts

--- views/ProposalView.tsx
+++ views/ProposalView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef, useMemo } from 'react';
+﻿import React, { useEffect, useState, useRef, useMemo } from 'react';
 import { jsPDF } from 'jspdf';
 import { BentoGrid, BentoCard } from '../components/BentoGrid';
 import { Button } from '../components/UI';
